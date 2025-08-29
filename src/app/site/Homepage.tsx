89fb97a62cbec47c4e4d102efdd9bf4895da30a1
Dev_Ak <aakash.velu.003@gmail.com>
2025-08-30 00:53:07 +0530

    changes

---
+++ src/app/site/Homepage.tsx
@@ -0,0 +1,82 @@
+import Image from "next/image";
+
+const pizzaData = [
+  {
+    name: "Focaccia",
+    ingredients: "Bread with italian olive oil and rosemary",
+    price: 6,
+    photoName: "pizzas/focaccia.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Margherita",
+    ingredients: "Tomato and mozarella",
+    price: 10,
+    photoName: "pizzas/margherita.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Spinaci",
+    ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+    price: 12,
+    photoName: "pizzas/spinaci.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Funghi",
+    ingredients: "Tomato, mozarella, mushrooms, and onion",
+    price: 12,
+    photoName: "pizzas/funghi.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Salamino",
+    ingredients: "Tomato, mozarella, and pepperoni",
+    price: 15,
+    photoName: "pizzas/salamino.jpg",
+    soldOut: true,
+  },
+  {
+    name: "Pizza Prosciutto",
+    ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+    price: 18,
+    photoName: "pizzas/prosciutto.jpg",
+    soldOut: false,
+  },
+];
+
+const Header = () => {
+  return <h1>Pizza store</h1>;
+};
+const Pizza = () => {
+  return (
+    <>
+      <h1>Pizza Menu</h1>
+      <Image src="/pizzas/focaccia.jpg" alt="image" width={300} height={200} />
+    </>
+  );
+};
+const Footer = () => {
+  const hour = new Date().getHours();
+  const openHour = 10;
+  const closeHour = 22;
+  const isOpen = hour >= openHour && hour <= closeHour;
+  console.log(isOpen);
+
+  return <p>{new Date().toLocaleTimeString()} We are open 10am - 12pm</p>;
+};
+
+export default function Homepage() {
+  return (
+    <section>
+      <div className="container ml-10 ">
+        <Header />
+        <Pizza />
+        <Pizza />
+        <Pizza />
+
+        <Footer />
+      </div>
+    </section>
+  );
+}
