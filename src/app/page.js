3fe3d3038d43c091f5de0c86c61a2e8f437039d8
Dev_Ak <aakash.velu.003@gmail.com>
2025-08-31 22:15:30 +0530

    Added few config files

--- src/app/page.js
+++ src/app/page.js
@@ -1,46 +1,46 @@
-import "./site/index.css";
+import './site/index.css';
 
 const pizzaData = [
   {
-    name: "Focaccia",
-    ingredients: "Bread with italian olive oil and rosemary",
+    name: 'Focaccia',
+    ingredients: 'Bread with italian olive oil and rosemary',
     price: 6,
-    photoName: "/pizzas/focaccia.jpg",
+    photoName: '/pizzas/focaccia.jpg',
     soldOut: false,
   },
   {
-    name: "Pizza Margherita",
-    ingredients: "Tomato and mozarella",
+    name: 'Pizza Margherita',
+    ingredients: 'Tomato and mozarella',
     price: 10,
-    photoName: "/pizzas/margherita.jpg",
+    photoName: '/pizzas/margherita.jpg',
     soldOut: false,
   },
   {
-    name: "Pizza Spinaci",
-    ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+    name: 'Pizza Spinaci',
+    ingredients: 'Tomato, mozarella, spinach, and ricotta cheese',
     price: 12,
-    photoName: "/pizzas/spinaci.jpg",
+    photoName: '/pizzas/spinaci.jpg',
     soldOut: false,
   },
   {
-    name: "Piza unghi",
-    ingredients: "Tomato, mozarella, mushrooms, and onion",
+    name: 'Piza unghi',
+    ingredients: 'Tomato, mozarella, mushrooms, and onion',
     price: 12,
-    photoName: "/pizzas/funghi.jpg",
+    photoName: '/pizzas/funghi.jpg',
     soldOut: false,
   },
   {
-    name: "Pizza Salmino",
-    ingredients: "Tomato, mozarella, and pepperoni",
+    name: 'Pizza Salmino',
+    ingredients: 'Tomato, mozarella, and pepperoni',
     price: 15,
-    photoName: "/pizzas/salamino.jpg",
+    photoName: '/pizzas/salamino.jpg',
     soldOut: true,
   },
   {
-    name: "Pizza Prosciutto",
-    ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+    name: 'Pizza Prosciutto',
+    ingredients: 'Tomato, mozarella, ham, aragula, and burrata cheese',
     price: 18,
-    photoName: "/pizzas/prosciutto.jpg",
+    photoName: '/pizzas/prosciutto.jpg',
     soldOut: false,
   },
 ];
@@ -57,7 +57,11 @@ const Menu = () => {
   return (
     <main className="menu">
       <h2>Our Menu</h2>
-      <ul className="">{pizzaData.map((pizza) => <Pizza pizzaObj={pizza} key={pizza.name} />)}</ul>
+      <ul className="">
+        {pizzaData.map(pizza => (
+          <Pizza pizzaObj={pizza} key={pizza.name} />
+        ))}
+      </ul>
 
       {/* <Pizza
         name="pizza spinac"
@@ -75,7 +79,7 @@ const Menu = () => {
   );
 };
 
-const Pizza = (props) => {
+const Pizza = props => {
   //   const style = { color: "red", fontSixze: "20px", textDecoration: "underline" };
   return (
     <li className="pizza">
@@ -98,7 +102,12 @@ const Footer = () => {
 
   return (
     <footer className="footer">
-      {new Date().toLocaleTimeString()} We are open 10am - 12pm
+      {isOpen && (
+        <div className="order">
+          <p>We are Open until {closeHour} garb fast!</p>
+          <button className="btn">order</button>
+        </div>
+      )}
     </footer>
   );
 };
